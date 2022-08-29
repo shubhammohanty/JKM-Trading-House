6dadc38ef34cedb2dda01f483073a2e8f496a22d
Shubham <whatsbehindit5@gmail.com>
2022-08-29 22:29:51 +0530

    project changed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# LibraryManagement
-Python Library Management System 
-Python project for Library Management System using mysql database written in python.
-This project is an inspiration for students studying in class 12 as this is a part of the investigatory project they have to submit. 
+# Trading House Financing System
+Python Trading House Financing System.
+Python project for Trading House Financing System using mysql database written in python.
+This project is an inspiration for students studying in class 12 as this is a part of the investigatory project they have to submit.
--- main.py
+++ main.py
@@ -1,2 +1,45 @@
 import datetime
-import mysql.connector
+import mysql.connector as MySQL
+
+def addwire(brand, length, gauge, bundles, dateof_purchase):
+    params = {
+    "host":"127.0.0.1", 
+    "user":"root", 
+    "password":"admin", 
+    "database":"jkm"
+    }
+    try:
+        con = MySQL.connect(**params)
+        cursor = con.cursor()
+        mySql_insert_query = """INSERT INTO wires (brand, length, gauge, bundles, dateof_purchase) 
+                                    VALUES (%s, %s, %s, %s, %s) """
+
+        record = (brand, length, gauge, bundles, dateof_purchase)
+        cursor.execute(mySql_insert_query, record)
+        con.commit()
+        print("Record inserted successfully into wires table")
+    except:
+        print("Failed to insert into MySQL table")
+    con.close()
+
+def addswitch(brand, amp, pieces, dateof_purchase):
+    params = {
+    "host":"127.0.0.1", 
+    "user":"root", 
+    "password":"admin", 
+    "database":"jkm"
+    }
+    try:
+        con = MySQL.connect(**params)
+        cursor = con.cursor()
+        mySql_insert_query = """INSERT INTO switches (brand, amp, pieces, dateof_purchase) 
+                                    VALUES (%s, %s, %s, %s) """
+
+        record = (brand, amp, pieces, dateof_purchase)
+        cursor.execute(mySql_insert_query, record)
+        con.commit()
+        print("Record inserted successfully into switches table")
+    except:
+        print("Failed to insert into MySQL table")
+    con.close()
+    
