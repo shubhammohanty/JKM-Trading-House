4b81fb2b9c125364fdb21136f7b8ef067111b530
Shubham <whatsbehindit5@gmail.com>
2022-08-29 22:57:22 +0530

    defined functions to add and display wires,switches

--- main.py
+++ main.py
@@ -43,3 +43,50 @@ def addswitch(brand, amp, pieces, dateof_purchase):
         print("Failed to insert into MySQL table")
     con.close()
     
+
+
+
+def showwire():
+    params = {
+    "host":"127.0.0.1", 
+    "user":"root", 
+    "password":"admin", 
+    "database":"jkm"
+    }
+    try:
+        con = MySQL.connect(**params)
+        cursor = con.cursor()
+        mySql_insert_query = "SELECT * FROM wires"
+        cursor.execute(mySql_insert_query)
+        records = cursor.fetchall()
+        for row in records:
+            print("Brand = ", row[0], )
+            print("Length = ", row[1])
+            print("Gauge  = ", row[2])
+            print("Bundles  = ", row[3])
+            print("Date Of Purchase  = ", row[4], "\n")
+    except:
+        print("Failed to display data in table wires")
+    con.close()
+
+def showswitch():
+    params = {
+    "host":"127.0.0.1", 
+    "user":"root", 
+    "password":"admin", 
+    "database":"jkm"
+    }
+    try:
+        con = MySQL.connect(**params)
+        cursor = con.cursor()
+        mySql_insert_query = "SELECT * FROM switches"
+        cursor.execute(mySql_insert_query)
+        records = cursor.fetchall()
+        for row in records:
+            print("Brand = ", row[0], )
+            print("Current Rating = ", row[1])
+            print("Pieces  = ", row[2])
+            print("Date Of Purchase  = ", row[3], "\n")
+    except:
+        print("Failed to display data in table switches")
+    con.close()
